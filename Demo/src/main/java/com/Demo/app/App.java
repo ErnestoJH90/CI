b7840f5ae95fb163a892da4a23fd3d0d8e75e060
Ernesto Everardo Jimenez Huitron <ernesto.jimenez@softtekprojects.com>
2021-10-18 11:45:42 -0500

    errors edited

--- Demo/src/main/java/com/Demo/app/App.java
+++ Demo/src/main/java/com/Demo/app/App.java
@@ -2,6 +2,6 @@ package org.Demo.app;
 
 public class App {
     public static void main(String[] args) {
-        logger.log.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
